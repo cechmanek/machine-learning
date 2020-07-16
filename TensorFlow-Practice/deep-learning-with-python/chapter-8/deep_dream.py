82ce6863217d26667fc70637c0987bd2634a0328
Justin Cechmanek <gj.cechmanek@gmail.com>
2020-07-16 16:06:21 -0700

    working deep dream implementation, after way way way too much mucking about

--- TensorFlow-Practice/deep-learning-with-python/chapter-8/deep_dream.py
+++ TensorFlow-Practice/deep-learning-with-python/chapter-8/deep_dream.py
@@ -9,6 +9,7 @@ instead here we maximize output of several layers, not just one
 
 # need a pretrained convnet. Inception was in the original paper so we'll use that
 import scipy as sc
+import numpy as np
 from tensorflow.keras.applications import inception_v3
 from tensorflow.keras.preprocessing import image
 import tensorflow.keras.backend as backend
@@ -16,6 +17,7 @@ import tensorflow.keras.backend as backend
 backend.set_learning_phase(0) # disable training on model as we don't need it
 
 model = inception_v3.InceptionV3(weights='imagenet', include_top=False)
+model.summary()
 
 # choose which layers we want to maximize the output of
 # lower layers lead to geometric patterns, higher layers lead to classes like dogs or birds
@@ -24,24 +26,32 @@ layer_contributions = {'mixed2':0.2, # some weighted average chosen rather arbit
                         'mixed4':2.0,
                         'mixed5':1.5}
 
-layer_dict = {[(layer.name, layer) for layer in lodel.layers]}
+# Get the symbolic outputs of each "key" layer (we gave them unique names).
+layer_dict = dict([(layer.name, layer) for layer in model.layers])
 
-loss = backend.variable() # the loss we'll use to minimize
-
-for layer_name in layer_contributions: # .keys()?
+# Define the loss.
+loss = backend.variable(0.0)
+for layer_name in layer_contributions:
+    # Add the L2 norm of the features of a layer to the loss.
     coeff = layer_contributions[layer_name]
     activation = layer_dict[layer_name].output
-    
-    # take L2 norm of the combined layer outputs as the loss 
+
+    # We avoid border artifacts by only involving non-border pixels in the loss.
     scaling = backend.prod(backend.cast(backend.shape(activation), 'float32'))
-    loss += coeff * backend.sum(backend.square(activation[:, 2: -2, 2:, -2, :])) / scaling    
+    #loss += coeff * backend.sum(backend.square(activation[:, 2: -2, 2: -2, :])) / scaling
+    # += operator is not supported on this TensorFlow version so do it longhand
+    loss = loss + coeff * backend.sum(backend.square(activation[:, 2: -2, 2: -2, :])) / scaling
+    
 
 # now we need the gradient ascent process to modify input image
-
+# This holds our generated image
 dream = model.input
 
+# Compute the gradients of the dream with regard to the loss.
 grads = backend.gradients(loss, dream)[0]
-grads /= backend.maximum(backend.mean(backend.abs(grads)), e-7) # normalize gradients
+
+# Normalize gradients.
+grads /= backend.maximum(backend.mean(backend.abs(grads)), 1e-7)
 
 outputs = [loss, grads]
 
@@ -50,17 +60,52 @@ fetch_loss_and_grads = backend.function([dream], outputs)
 def eval_loss_and_grads(x):
     outs = fetch_loss_and_grads([x])
     loss_value = outs[0]
-    grad_value = outs[1]
+    grad_values = outs[1]
     return loss_value, grad_values
 
 def gradient_ascent(x, iterations, step, max_loss=None):
     for i in range(iterations):
-        loss_value, grad_vales -= eval_loss_and_grads(x)
-            if max_loss is not None and loss_value > max_Loss:
-                break
-            print('...Loss value at', i, ':', loss_value)
-            x += step * grad_values
-        return x
+        loss_value, grad_values = eval_loss_and_grads(x)
+        if max_loss is not None and loss_value > max_loss:
+            break
+        print('...Loss value at', i, ':', loss_value)
+        x += step * grad_values
+    return x
+
+# we'll need some helper functions to handle the image
+
+def resize_image(img, size):
+    img = np.copy(img) # local copy to not modify original
+    factors = (1, float(size[0]) / img.shape[1], float(size[1]) / img.shape[2], 1)
+    return sc.ndimage.zoom(img, factors, order=1)
+    
+def save_image(img, filename):
+    #pil_img = deprocess_image(np.copy(img)) # not a PIL image, Chollet you fucking moron
+    #sc.misc.imsave(filename, pil_img) # imsave() removed years ago. Eat shit Chollet 
+    img = deprocess_image(np.copy(img))
+    image.save_img(filename, img)
+    
+
+def preprocess_image(image_path):
+    img = image.load_img(image_path)
+    img = image.img_to_array(img)
+    img = np.expand_dims(img, axis=0)
+    img = inception_v3.preprocess_input(img)
+    return img
+
+def deprocess_image(x):
+    if backend.image_data_format() == 'channels_first':
+        x = x.reshape((3, x.shape[2], x.shape[1]))
+        x = x.transpose((1,2,0))
+    else:
+        x = x.reshape((x.shape[1], x.shape[2], 3))
+    x /= 2.0
+    x += 0.5
+    x *= 255.0
+    x = np.clip(x, 0, 255).astype('uint8')
+    
+    return x
+
 
 # heart of Deep Dream algorithm involves processing input image at different scales called octaves
 # after each processing step we re-inject the details of original image that were lost
@@ -76,10 +121,9 @@ max_loss = 10.0 # if loss>10 interupt gradient ascent to stop weird artifacts
 base_image_path = 'outdoor_cat.jpg'
 cat_image = preprocess_image(base_image_path)
 
-
 original_shape = cat_image.shape[1:3] # first col is batch size
-sucessive_shapes = [original_shape]
-for i in range(1, num_octave):
+successive_shapes = [original_shape]
+for i in range(1, num_octaves):
     shape = tuple([int(dim / (octave_scale ** 1)) for dim in original_shape])
     successive_shapes.append(shape) # list of shape tuples to run gradient ascent at
 
@@ -87,7 +131,7 @@ successive_shapes = successive_shapes[::-1] # reverse so they're in increasing o
 
 original_image = np.copy(cat_image)
 
-shrunk_original_image = resize_image(img, successive_shapes[0])
+shrunk_original_image = resize_image(cat_image, successive_shapes[0])
 
 for shape in successive_shapes:
     print('Processing image shape', shape)
@@ -96,44 +140,12 @@ for shape in successive_shapes:
     # run gradient ascent to modify input image
     img = gradient_ascent(img, iterations=iterations, step=step, max_loss=max_loss)
     
-    upscaled_shrunk_orginial_image = resize_image(shrunk_original_image, shape)
-    same_size_orignial = resize_image(original_image, shape)
+    upscaled_shrunk_original_image = resize_image(shrunk_original_image, shape)
+    same_size_original = resize_image(original_image, shape)
     lost_detail = same_size_original - upscaled_shrunk_original_image
     img += lost_detail # add back in detail we lost during scale up
 
-    shrunk_original_image = resize_image(orignial_image, shape)
+    shrunk_original_image = resize_image(original_image, shape)
     save_image(img, filename='final_dream.png')
-
-
-# we'll need some helper functions to handle the image
-
-def resize_image(img, size):
-    img = np.copy(img) # local copy to not modify original
-    factors = (1, float(size[0]) / img.shape[1], float(size[1]) / img.shape[2], 1)
-    return sc.ndimage.zoom(img, factors, order=1)
-    
-def save_image(img, filename):
-    pil_img = deprocess_image(np.copy(img))
-    sc.misc.imsave(filename, pil_img)
-
-def preprocess_image(image_path):
-    img = image.load_img(image_path)
-    img = image.image_to_array(img)
-    img = np.expand_dims(img, axis=0)
-    img = inception_v3.preprocess_input(img)
-    return img
-
-def deprocess_image(x):
-    if backend.image_dat_format() == 'channels_first':
-        x = x.reshpae((3, x.shape[2], x.shape[1]))
-        x = x.transpose((1,2,0))
-    else:
-        x = x.reshape((x.shape[1], x.shape[2], 3))
-    x /= 2.0
-    x += 0.5
-    x *= 255.0
-    x = np.clip(x, 0, 255).astype('uint8')
-    
-    return x
 
 
