fd8d148d374be95e3e368f3afe1a45ebd29344ad
justin cechmanek <gj.cechmanek@gmail.com>
2020-09-13 17:04:08 -0700

    adds replay buffer class

--- reinforcement-learning/deep-rl-hands-on/chapter-6/dqn_pong.py
+++ reinforcement-learning/deep-rl-hands-on/chapter-6/dqn_pong.py
@@ -26,3 +26,25 @@ EPSILON_START = 1.0 # epsilon is probability of choosing random actions. Used fo
 EPSILON_FINAL = 0.02 # as we learn more reduce the probability of random actions to this value
 
 
+Experience = collections.namedtuple('Experience',
+                                    field_names=['state','action','reward','done', 'new_state'])
+
+
+class ExperienceBuffer():
+  def __init__(self, capacity):
+    self.buffer = collections.deque(maxlen=capacity)
+
+  def __len__(self):
+    return len(self.buffer)
+  
+  def append(self, experience):
+    self.buffer.append(experience)
+
+
+  def sample(self, batch_size):
+    # randomly sample some experiences. This ensures we see things out of order while training
+    indices = np.random.choice(len(self.buffer), batch_size, replace=False)
+    states, actions, rewards, dones, next_states = zip(*[self.buffer[idx] for idx in indices])
+    return np.array(states), np.array(actions), np.array(rewards, dtype=np.float32), np.array(dones, dtype=np.uint8), np.array(next_states)
+  
+  
