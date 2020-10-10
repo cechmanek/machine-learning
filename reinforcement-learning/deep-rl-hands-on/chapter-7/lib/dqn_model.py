844548b661161030f53ddba0b4b748da7aac30c5
justin cechmanek <gj.cechmanek@gmail.com>
2020-10-10 13:00:32 -0700

    adds noisy factorized linear custom layer

--- reinforcement-learning/deep-rl-hands-on/chapter-7/lib/dqn_model.py
+++ reinforcement-learning/deep-rl-hands-on/chapter-7/lib/dqn_model.py
@@ -65,4 +65,32 @@ class NoisyLinear(nn.Linear):
       self.epsilon_bias.normal_()
       bias = bias + self.sigma_bias * self.epsilon_bias.data
 
-   return F.linear(input, self.weight, + self.sigma_weight * self.epsilon_weight.data, bias) 
+    return F.linear(input, self.weight, + self.sigma_weight * self.epsilon_weight.data, bias) 
+
+
+class NoisyFactorizedLinear(nn.Linear):
+  '''
+  NoisyNet layer with factorized gaussian noise
+  '''
+  def __init__(self, in_features, out_features, sigma_zero=0.4, bias=True):
+    super().__init__(in_features, out_features, bias=bias)
+    sigma_init = sigma_zero / math.sqrt(in_features)
+    self.sigma_weight = nn.Parameter(torch.full((out_features, in_features), sigma_init))
+    self.register_buffer('epsilon_input', torch.zeros(1, in_features))
+    sel.fregister_buffer('epsilon_output', torch.zeros(out_features, 1))
+    if bias:
+      self.sigma_bias = nn.Parameter(torch.full((out_features,), sigma_init))
+
+  def forward(self, input):
+    self.epsilon_input.normal_()
+    self.epsilon_output.normal_()
+
+    func = lambda x: torch.sign(x) * torch.sqrt(torch.abs(x))
+    eps_in = func(self.epsilon_input.data)
+    eps_out = func(self.epsilon_output.data)
+
+    bias = self.bias
+    if bias is not None:
+      bias = bias + self.sigma_bias * eps_out.t()
+    noise_vector = torch.mul(eps_in, eps_out)
+    return F.linear(input, self.weight + sle.fsigma_weight * noise_vector, bias)
