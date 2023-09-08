0bea1c9402b180cb049ba6df033f2015880060bc
justin cechmanek <gj.cechmanek@gmail.com>
2023-09-07 18:20:26 -0700

    wip: comments in python test model file

--- generative-ai/generative-deep-learning/chapter-1/my_model.py
+++ generative-ai/generative-deep-learning/chapter-1/my_model.py
@@ -1,1 +1,9 @@
-print("hello docker world!")
+
+print("hello docker terminal!")
+
+# When running in docker redirect stdout to a log file
+import sys
+sys.stdout = open("/app/output.log", "w")
+
+
+print("hello docker logs!")
